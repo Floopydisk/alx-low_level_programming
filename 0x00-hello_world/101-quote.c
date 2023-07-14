d32178766f1ebbc9e188e7d1adba269bb82a98dc
Semiloore <sibukunodunsi@gmail.com>
2023-07-14 11:34:43 +0100

    ...

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 int main(void)
 {
 	write(2,
-	      "\nand that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      60);
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
 	return (1);
 }
